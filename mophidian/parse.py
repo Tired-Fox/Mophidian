1420b52c5a4f7f6c28a30b4b5f5333be111b73f0
Zachary Boehm <zboehm104@gmail.com>
2022-10-12 18:35:22 -0500

    parse basic md file

--- mophidian/parse.py
+++ mophidian/parse.py
@@ -1,1 +1,29 @@
 import markdown
+import json
+
+with open('config.json', 'r', encoding='utf-8') as config_file:
+    cfx = json.load(config_file)
+
+with open('sample.md', 'r', encoding='utf-8') as mark_file:
+    content = mark_file.read()
+
+md = markdown.Markdown(extensions=cfx["markdown"]["extensions"], extension_configs=cfx["markdown"]["extension_configs"])
+
+html = md.reset().convert(content)
+
+with open('sample.html', '+w', encoding="utf-8") as html_file:
+    html_file.write(f'''\
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    {html}
+</body>
+</html>\
+''')
---
+++ mophidian/sample.md
@@ -0,0 +1,13 @@
+# Heading 1
+
+Some text under heading 1
+
+## Heading 2
+
+|one|two|
+|:-:|:--|
+|red|blue|
+
+footnote[^1]
+
+[^1]: Footnote text is here at the bottom. It includes a backlink
